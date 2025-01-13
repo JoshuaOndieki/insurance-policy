fb98166adeea40378fd6e90b80b56127d1e7aa9a
Joshua Ondieki <oj@joshuaondieki.com>
2025-01-13 08:00:26 +0300

    update prod api url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {baseEnvironment} from './environment.base';
 export const environment = {
   ...baseEnvironment,
   production: true,
-  APIUrl: 'https://insure-api.ojapps.com/',
+  APIUrl: 'https://insure-api.ojapps.com:8008/',
 }
